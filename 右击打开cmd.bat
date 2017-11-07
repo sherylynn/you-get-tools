0ee573e6ea4355df138ea0e8b8c77e4ce6910280
sherylynn <352281674@qq.com>
2017-11-07 19:09:28 +0800

    新建一些批处理

---
+++ 右击打开cmd.bat
@@ -0,0 +1,26 @@
+﻿@echo off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+@echo off
+cls
+setlocal EnableDelayedExpansion
+rem %~fs0是执行文件 %~dp0是文件夹
+
+Reg add HKCR\Directory\shell\OpenCmdHere /ve /t REG_SZ /d "open in cmd" /f
+Reg add HKCR\Directory\shell\OpenCmdHere /v "Icon" /t REG_SZ /d "cmd.exe" /f
+Reg add HKCR\Directory\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe /s /k pushd "%V"" /f
+
+Reg add HKCR\Directory\Background\shell\OpenCmdHere /ve /t REG_SZ /d "open in cmd" /f
+Reg add HKCR\Directory\Background\shell\OpenCmdHere /v "Icon" /t REG_SZ /d "cmd.exe" /f
+Reg add HKCR\Directory\Background\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe /s /k pushd "%V"" /f
+
+Reg add HKCR\Drive\shell\OpenCmdHere /ve /t REG_SZ /d "open in cmd" /f
+Reg add HKCR\Drive\shell\OpenCmdHere /v "Icon" /t REG_SZ /d "cmd.exe" /f
+Reg add HKCR\Drive\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe /s /k pushd "%V"" /f
+
+Reg add HKCR\Directory\LibraryFolder\background\shell\OpenCmdHere /ve /t REG_SZ /d "open in cmd" /f
+Reg add HKCR\Directory\LibraryFolder\background\shell\OpenCmdHere /v "Icon" /t REG_SZ /d "cmd.exe" /f
+Reg add HKCR\Directory\LibraryFolder\background\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe /s /k pushd "%V"" /f
+
+echo test
+pause
