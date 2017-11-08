6a383ca3e16889632a7a4ec09e573addd22408f5
sherylynn <352281674@qq.com>
2017-11-08 10:12:46 +0800

    新建了一些删除命令

---
+++ 删除右击的git命令.bat
@@ -0,0 +1,15 @@
+﻿@echo off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+@echo off
+cls
+setlocal EnableDelayedExpansion
+Reg delete HKCR\Directory\shell\git_gui  /f
+Reg delete HKCR\Directory\Background\shell\git_gui  /f
+Reg delete HKCR\Drive\shell\git_gui  /f
+Reg delete HKCR\Directory\LibraryFolder\background\shell\git_gui  /f
+Reg delete HKCR\Directory\shell\git_shell  /f
+Reg delete HKCR\Directory\Background\shell\git_shell  /f
+Reg delete HKCR\Drive\shell\git_shell  /f
+Reg delete HKCR\Directory\LibraryFolder\background\shell\git_shell  /f
+pause
