45872362af0300273fd1103c8d7520e119dec5b8
sherylynn <352281674@qq.com>
2017-11-08 08:40:47 +0800

    修改了打开方式

--- 右击打开cmd - 副本.bat
+++ 右击打开cmd - 副本.bat
@@ -9,19 +9,19 @@ rem %~fs0是执行文件 %~dp0是文件夹
 rem cmd /k 是不提示版本号
 Reg add HKCR\Directory\shell\OpenCmdHere /ve /t REG_SZ /d "open in cmd" /f
 Reg add HKCR\Directory\shell\OpenCmdHere /v "Icon" /t REG_SZ /d "cmd.exe" /f
-Reg add HKCR\Directory\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe /k" /f
+Reg add HKCR\Directory\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe" /f
 
 Reg add HKCR\Directory\Background\shell\OpenCmdHere /ve /t REG_SZ /d "open in cmd" /f
 Reg add HKCR\Directory\Background\shell\OpenCmdHere /v "Icon" /t REG_SZ /d "cmd.exe" /f
-Reg add HKCR\Directory\Background\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe /k" /f
+Reg add HKCR\Directory\Background\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe" /f
 
 Reg add HKCR\Drive\shell\OpenCmdHere /ve /t REG_SZ /d "open in cmd" /f
 Reg add HKCR\Drive\shell\OpenCmdHere /v "Icon" /t REG_SZ /d "cmd.exe" /f
-Reg add HKCR\Drive\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe /k" /f
+Reg add HKCR\Drive\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe" /f
 
 Reg add HKCR\Directory\LibraryFolder\background\shell\OpenCmdHere /ve /t REG_SZ /d "open in cmd" /f
 Reg add HKCR\Directory\LibraryFolder\background\shell\OpenCmdHere /v "Icon" /t REG_SZ /d "cmd.exe" /f
-Reg add HKCR\Directory\LibraryFolder\background\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe /k" /f
+Reg add HKCR\Directory\LibraryFolder\background\shell\OpenCmdHere\command /ve /t REG_SZ /d "cmd.exe" /f
 
 echo test
 pause
